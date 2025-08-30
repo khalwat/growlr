6d40fc1dea75628cd0b02be35f1a43b1e817589b
Andrew Welch <andrew@nystudio107.com>
2025-08-30 16:12:23 -0400

    feat: Bootstrap module

--- config/app.php
+++ config/app.php
@@ -21,7 +21,14 @@
  */
 
 use craft\helpers\App;
+use modules\sitemodule\SiteModule;
 
 return [
     'id' => App::env('CRAFT_APP_ID') ?: 'CraftCMS',
+    'modules' => [
+        'site-module' => [
+            'class' => SiteModule::class,
+        ],
+    ],
+    'bootstrap' => ['site-module'],
 ];
